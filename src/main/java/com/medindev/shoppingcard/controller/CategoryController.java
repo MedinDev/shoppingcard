63d79243d67b81a03d71a773500fbb499ae1b835
medtexprog <medtexprog@gmail.com>
2025-07-06 15:03:06 +0800

    day2

---
+++ src/main/java/com/medindev/shoppingcard/controller/CategoryController.java
@@ -0,0 +1,9 @@
+package com.medindev.shoppingcard.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")  // Base URL for all category-related endpoints
+public class CategoryController {
+}
